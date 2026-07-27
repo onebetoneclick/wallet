1e8236d5c473e37107837d649dc03b107fb83820
Don Martial <onebet.oneclick@gmail.com>
2026-07-27 13:53:34 +0100

    Add BVN customer identification payload fields

--- services/paystack.js
+++ services/paystack.js
@@ -51,11 +51,13 @@ async function identifyCustomer(customerCode, data) {
         {
             country: "NG",
             type: "bank_account",
+            value: data.bvn,
             account_number: data.account_number,
             bvn: data.bvn,
             bank_code: data.bank_code,
             first_name: data.first_name,
-            last_name: data.last_name
+            last_name: data.last_name,
+            middle_name: data.middle_name || ""
         }
     );
 
