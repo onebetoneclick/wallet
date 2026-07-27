25d68c75342ec1d602a316fae5fe7330d26759a4
Don Martial <onebet.oneclick@gmail.com>
2026-07-27 11:53:49 +0100

    Create paystack.js

---
+++ services/paystack.js
@@ -0,0 +1,179 @@
+require("dotenv").config();
+
+const axios = require("axios");
+
+/*
+|--------------------------------------------------------------------------
+| Paystack Configuration
+|--------------------------------------------------------------------------
+*/
+
+const PAYSTACK_SECRET_KEY = process.env.PAYSTACK_SECRET_KEY;
+
+if (!PAYSTACK_SECRET_KEY) {
+    throw new Error("PAYSTACK_SECRET_KEY is missing in .env");
+}
+
+const paystack = axios.create({
+    baseURL: "https://api.paystack.co",
+    headers: {
+        Authorization: `Bearer ${PAYSTACK_SECRET_KEY}`,
+        "Content-Type": "application/json"
+    }
+});
+
+/*
+|--------------------------------------------------------------------------
+| Create Customer
+|--------------------------------------------------------------------------
+*/
+
+async function createCustomer(data) {
+
+    const response = await paystack.post("/customer", {
+        email: data.email,
+        first_name: data.first_name,
+        last_name: data.last_name,
+        phone: data.phone
+    });
+
+    return response.data.data;
+
+}
+
+/*
+|--------------------------------------------------------------------------
+| Create Dedicated Account (NUBAN)
+|--------------------------------------------------------------------------
+*/
+
+async function createDedicatedAccount(customerCode) {
+
+    const response = await paystack.post("/dedicated_account", {
+        customer: customerCode,
+        preferred_bank: "wema-bank"
+    });
+
+    return response.data.data;
+
+}
+
+/*
+|--------------------------------------------------------------------------
+| Verify Bank Account
+|--------------------------------------------------------------------------
+*/
+
+async function verifyAccount(accountNumber, bankCode) {
+
+    const response = await paystack.get(
+        `/bank/resolve?account_number=${accountNumber}&bank_code=${bankCode}`
+    );
+
+    return response.data.data;
+
+}
+
+/*
+|--------------------------------------------------------------------------
+| List Banks
+|--------------------------------------------------------------------------
+*/
+
+async function getBanks() {
+
+    const response = await paystack.get("/bank");
+
+    return response.data.data;
+
+}
+
+/*
+|--------------------------------------------------------------------------
+| Create Transfer Recipient
+|--------------------------------------------------------------------------
+*/
+
+async function createTransferRecipient(data) {
+
+    const response = await paystack.post("/transferrecipient", {
+
+        type: "nuban",
+
+        name: data.name,
+
+        account_number: data.account_number,
+
+        bank_code: data.bank_code,
+
+        currency: "NGN"
+
+    });
+
+    return response.data.data;
+
+}
+
+/*
+|--------------------------------------------------------------------------
+| Initiate Transfer
+|--------------------------------------------------------------------------
+*/
+
+async function initiateTransfer(data) {
+
+    const response = await paystack.post("/transfer", {
+
+        source: "balance",
+
+        amount: data.amount, // Kobo
+
+        recipient: data.recipient_code,
+
+        reason: data.reason
+
+    });
+
+    return response.data.data;
+
+}
+
+/*
+|--------------------------------------------------------------------------
+| Verify Transaction
+|--------------------------------------------------------------------------
+*/
+
+async function verifyTransaction(reference) {
+
+    const response = await paystack.get(
+        `/transaction/verify/${reference}`
+    );
+
+    return response.data.data;
+
+}
+
+/*
+|--------------------------------------------------------------------------
+| Export
+|--------------------------------------------------------------------------
+*/
+
+module.exports = {
+
+    createCustomer,
+
+    createDedicatedAccount,
+
+    verifyAccount,
+
+    getBanks,
+
+    createTransferRecipient,
+
+    initiateTransfer,
+
+    verifyTransaction
+
+};
