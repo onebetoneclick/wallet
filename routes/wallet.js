38ac6190aad6f6c2b66593ca521cba7ffe20b4fd
Don Martial <onebet.oneclick@gmail.com>
2026-07-29 14:33:04 +0100

    Update wallet.js

--- routes/wallet.js
+++ routes/wallet.js
@@ -12,7 +12,9 @@ const {
 
     activateWallet,
 
-    verifyUser
+    verifyUser,
+
+    getWallet
 
 } = require("../controllers/walletController");
 
@@ -53,6 +55,14 @@ router.post("/verify-activation-otp", verifyActivationOTP);
 | Activate Wallet
 |--------------------------------------------------------------------------
 */
+
+/*
+|--------------------------------------------------------------------------
+| Get Existing Wallet
+|--------------------------------------------------------------------------
+*/
+
+router.post("/get-wallet", getWallet);
 
 router.post("/activate", activateWallet);
 
