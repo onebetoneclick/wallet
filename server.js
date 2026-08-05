6319e913402e14ede2d82bfd231827b472acffaf
Don Martial <onebet.oneclick@gmail.com>
2026-08-05 12:07:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -21,10 +21,6 @@ const app = express();
 
 app.use(cors());
 
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
-
 /*
 |--------------------------------------------------------------------------
 | Webhook
